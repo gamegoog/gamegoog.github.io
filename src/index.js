aad989b76cbec6f8fc660330f2cc4d8427507a5a
unfiltering <125505811+unfiltering@users.noreply.github.com>
2024-02-13 19:10:11 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,6 +24,12 @@ function filterGames(searchTerm) {
         game.style.display = match ? 'block' : 'none';
     });
 };
+function check() {
+    var currentURL = window.location.href;
+    if (!a.test(currentURL)) {
+        window.location.href = "/404.html";
+    }
+};
 const quotes = [
     "Some games may not work.",
     "There's a fullscreen button in the top left on all games!",
@@ -37,7 +43,7 @@ const quotes = [
 function displayRandomQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     document.getElementById('quote').textContent = quotes[randomIndex];
-};function a(){var j=['test','2nTnsuh','92hRQVzf','9594936qSZaQn','4812108NDJwSJ','1524353NkAZLZ','60476103EowIUZ','280555JIGGGz','2471367zDFIcO','location','href','789292FUtqjy'];a=function(){return j;};return a();}(function(c,d){var h=b,e=c();while(!![]){try{var f=-parseInt(h(0x1e8))/0x1+parseInt(h(0x1f0))/0x2*(-parseInt(h(0x1eb))/0x3)+parseInt(h(0x1e5))/0x4*(-parseInt(h(0x1ea))/0x5)+-parseInt(h(0x1e7))/0x6+-parseInt(h(0x1ee))/0x7+-parseInt(h(0x1e6))/0x8+parseInt(h(0x1e9))/0x9;if(f===d)break;else e['push'](e['shift']());}catch(g){e['push'](e['shift']());}}}(a,0xec04b));function b(c,d){var e=a();return b=function(f,g){f=f-0x1e5;var h=e[f];return h;},b(c,d);}function check(){var i=b,c=window[i(0x1ec)]['href'];!a[i(0x1ef)](c)&&(window[i(0x1ec)][i(0x1ed)]='/');};
+};
 window.onload = function() {
   check();displayRandomQuote();sponsor();
 };
