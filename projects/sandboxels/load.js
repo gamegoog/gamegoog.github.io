76e08863d30c22b050937c52312a12a894b44b21
unfiltering <125505811+unfiltering@users.noreply.github.com>
2024-02-16 23:05:27 -0500

    Update load.js

--- projects/sandboxels/load.js
+++ projects/sandboxels/load.js
@@ -81,7 +81,7 @@ function addSchema(schema,id) {
 window.addEventListener("load",function(){
 
 // Console Watermark
-console.log("%c WELCOME TO R74n ","position: absolute; top: 50%; right: 50%; transform: translate(50%,-50%); font-family: Arial; font-size: 3em; font-weight: 700; color: #00ffff; text-shadow: 1px 1px 1px #14c9c9, 1px 2px 1px #14c9c9, 1px 3px 1px #14c9c9, 1px 4px 1px #14c9c9, 1px 5px 1px #14c9c9, 1px 13px 6px rgba(16,16,16,0.4), 1px 22px 10px rgba(16,16,16,0.2), 1px 25px 35px rgba(16,16,16,0.2), 1px 30px 60px rgba(16,16,16,0.4);padding:10px")
+console.log("%c WELCOME TO R74n! MODDED BY GLOABE.GITHUB.IO, ","position: absolute; top: 50%; right: 50%; transform: translate(50%,-50%); font-family: Arial; font-size: 3em; font-weight: 700; color: #00ffff; text-shadow: 1px 1px 1px #14c9c9, 1px 2px 1px #14c9c9, 1px 3px 1px #14c9c9, 1px 4px 1px #14c9c9, 1px 5px 1px #14c9c9, 1px 13px 6px rgba(16,16,16,0.4), 1px 22px 10px rgba(16,16,16,0.2), 1px 25px 35px rgba(16,16,16,0.2), 1px 30px 60px rgba(16,16,16,0.4);padding:10px")
 
 function callRue() {
 	if (typeof Rue === "undefined" && !document.getElementById("rueScript")) {
