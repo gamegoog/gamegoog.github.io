74d45751ad70b0287d5f81f556ba8796150f3056
unfiltering <125505811+unfiltering@users.noreply.github.com>
2024-02-29 19:58:54 -0500

    Update slots.js

--- projects/gamblr/js/slots.js
+++ projects/gamblr/js/slots.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const textures = ['cherry', 'lemon', 'orange', 'plum', 'bell', 'bar'];
+    const textures = ['cherry', 'lemon', 'orange', 'plum', 'bell', 'bar', 'bar', 'bar', 'bar', 'bar'];
     const moneyDisplay = document.getElementById('money');
     const betInput = document.getElementById('bet');
     const slotImages = [
