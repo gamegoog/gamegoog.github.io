430d3654fdeefdb38fe1c24970e578a6fbaefca7
unfiltering <125505811+unfiltering@users.noreply.github.com>
2024-03-07 14:23:09 -0500

    Update crash.js

--- gamblr/js/crash.js
+++ gamblr/js/crash.js
@@ -2,7 +2,7 @@ const SITE_DISABLED = 0;
 const CRASH_CHANCE = 0.4999998;
 const ROCKET_SPEED = 2000; // ms
 const RESPAWN_SPEED = 840; // ms
-const ERROR_AWARD = 5.8; // The amount of times cash is multiplied if the cash is below the bet amount
+const ERROR_AWARD = 3.4; // The amount of times cash is multiplied if the cash is below the bet amount
 
 document.addEventListener('DOMContentLoaded', () => {
     initialize(); // Call initialize function when the DOM content is loaded
